d7f9bb28806118aa869c75f7ff3a02664ea2f5c6
harish9312 <hksoni.9312@gmail.com>
2016-10-02 15:28:41 +0530

    Update Reverse.java

--- Reverse.java
+++ Reverse.java
@@ -2,11 +2,11 @@ import java.util.Scanner;
 class Reverse{
 public static void main(String[] args){
 Scanner sc=new Scanner (System.in);
-int no,rev=0;
+int m=0,no,rev=0;
 no=sc.nextInt();
 while(no!=0){
-rev=rev%10;
-rev=rev/10;
+m=no%10;
+rev=rev*10+m;
 no=no/10;
 }
  System.out.println("Reverse of entered number is "+rev);
